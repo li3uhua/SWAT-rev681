5c3dced46e58bd8023c6d99c7236f4aaddd3fa34
li3uhua <li3uhua@qq.com>
2022-08-24 16:05:24 +0200

    sas I/O for nitrogen

--- nlch.f
+++ nlch.f
@@ -154,6 +154,8 @@
       !! calculate nitrate leaching from soil profile
       percn(j) = percnlyr
 
+      !! sas nutient input (convert kg/ha to mg/L)
+      sas_cqin_hru(j) = sas_cqin_hru(j) + percn(j)/sas_cqin(j)*100
 
       nloss = (2.18 * dis_stream(j) - 8.63) / 100.
       nloss = dmax1(0.,nloss)
