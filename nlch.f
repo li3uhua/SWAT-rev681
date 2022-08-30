8b3ef39a6d33b661cfdc7db1a178be771fa18392
li3uhua <li3uhua@qq.com>
2022-08-30 14:31:54 +0200

    little change of nlch.f

--- nlch.f
+++ nlch.f
@@ -159,7 +159,7 @@
       if (sas_qin_hru(j) < 0.01) then
         sas_cqin_hru(j) = 0.0
       else
-        sas_cqin_hru(j) = sas_cqin_hru(j) + percn(j)/sas_qin_hru(j)*100
+        sas_cqin_hru(j) = sas_cqin_hru(j) + (percn(j)/sas_qin_hru(j))*100
       end if
 
       nloss = (2.18 * dis_stream(j) - 8.63) / 100.
